03f5473e1131ffe9244718b14c3ccb7ec553b36a
Ilya <72245457+terrorChay@users.noreply.github.com>
2023-01-15 14:25:30 +0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -172,7 +172,7 @@ def main():
             _fields_df = _fields_df.drop_duplicates(subset='Микро-направление')
             _fields_df = _fields_df.merge(_fields_count, left_on='Микро-направление', right_on='index').drop(labels='index', axis=1)
             fig = px.sunburst(_fields_df,
-            path                    = ['Макро', 'Микро'],
+            path                    = ['Макро-направление', 'Микро-направление'],
             values                  = 'Количество',
             branchvalues            = "total",
             color_discrete_sequence = colors
