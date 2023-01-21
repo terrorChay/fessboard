ccfde0f9a71a33a031ec89259088840d84c79442
Ilya <72245457+terrorChay@users.noreply.github.com>
2023-01-21 22:20:49 +0300

    Update my_query.py

--- my_query.py
+++ my_query.py
@@ -8,13 +8,12 @@ query_dict =    {
                                                 projects.project_start_date AS `Дата начала`,
                                                 projects.project_end_date AS `Дата окончания`,
                                                 CASE
-                                                    WHEN ISDATE(projects.project_start_date) <> 1
-                                                        THEN projects.project_start_date
+                                                    WHEN YEAR(projects.project_start_date) = 0
+                                                        THEN NULL
                                                     WHEN MONTH(projects.project_start_date) > 8
-                                                        THEN 'cur - higher'
-                                                    ELSE
-                                                        THEN 'lower - cur'
-                                                END AS 'Академический год',
+                                                        THEN CONCAT_WS(' - ', YEAR(projects.project_start_date), YEAR(projects.project_start_date)+1)
+                                                    ELSE CONCAT_WS(' - ', YEAR(projects.project_start_date)-1, YEAR(projects.project_start_date))
+                                                END AS `Академический год`,
                                                 companies.company_name AS `Название компании`,
                                                 company_types.company_type AS `Тип компании`,
                                                 company_spheres.company_sphere AS Отрасль,
