2c1f2a625124656871ded9e2f249cf7767a7a4dc
Ilya <72245457+terrorChay@users.noreply.github.com>
2023-03-13 13:42:26 +0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -25,24 +25,24 @@ config = {'staticPlot': False,'displayModeBar': False}
 
 def main():
     # load data
-    with st.spinner('Читаем PMI и PMBOK...'):
+    with st.spinner('(1/8) Читаем PMI и PMBOK...'):
         projects_df             = utils.load_projects()
-    with st.spinner('Происходит аджайл...'):
+    with st.spinner('(2/8) Происходит аджайл...'):
         students_in_projects_df     = utils.load_students_in_projects()
         moderators_in_projects_df   = students_in_projects_df.loc[students_in_projects_df['Модератор'] == 1]
         curators_in_projects_df     = students_in_projects_df.loc[students_in_projects_df['Куратор'] == 1]
         students_in_projects_df     = students_in_projects_df.loc[(students_in_projects_df['Куратор'] == 0) & (students_in_projects_df['Модератор'] == 0)]
-    with st.spinner('Изучаем требования стейкхолдеров...'):
+    with st.spinner('(3/8) Изучаем требования стейкхолдеров...'):
         teachers_in_projects_df = utils.load_teachers_in_projects()
-    with st.spinner('Еще чуть-чуть и прямо в рай...'):
+    with st.spinner('(4/8) Еще чуть-чуть и прямо в рай...'):
         students_df             = utils.load_students()
-    with st.spinner('Нежно обращаемся к базе данных...'):
+    with st.spinner('(5/8) Нежно обращаемся к базе данных...'):
         universities_df         = utils.load_universities()
-    with st.spinner('Собираем встречу выпускников...'):
+    with st.spinner('(6/8) Собираем встречу выпускников...'):
         events_df               = utils.load_events()
-    with st.spinner('Советуемся с ChatGPT...'):
+    with st.spinner('(7/8) Советуемся с ChatGPT...'):
         students_in_events_df   = utils.load_students_in_events()
-    with st.spinner('Звоним представителям компаний...'):
+    with st.spinner('(8/8) Звоним представителям компаний...'):
         companies_df   = utils.load_companies()
     
     selection = st.sidebar.selectbox(options =color_themes.keys(),label='Выберите тему')
