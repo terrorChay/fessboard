4cfd7d9825983a1b9962235e7f8e9483d4923d6a
MtankoAwara <mikhail.tanko@awara-it.com>
2023-03-26 20:45:32 +0300

    Lots of stuff

--- django_app/fessboard/homepage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import index
-from .views import homepage
-
-urlpatterns = [
-    path('', homepage),
-    path('company-form/', index, name='company-form'),
-    #path('to-form/',)
-]
