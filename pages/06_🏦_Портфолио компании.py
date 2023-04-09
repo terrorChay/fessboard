906b3d7764861e4f6d4f2f9c4c85beb96bb2c3c5
Ilya <72245457+terrorChay@users.noreply.github.com>
2023-04-09 21:03:45 +0300

    Update 06_🏦_Портфолио компании.py

--- pages/06_🏦_Портфолио компании.py
+++ pages/06_🏦_Портфолио компании.py
@@ -19,149 +19,35 @@ color_themes = {
                 'ЦПР'    :['#3A42FF','#00046F','#2227A7','#88B1FF','#D3E2FF','#C0C0C0','#969696','#5B5B5B','#222222','#FFFFFF','#FB832A']
                 }
 tr='rgba(0,0,0,0)'
-
-
-
-
 font="Source Sans Pro"
 config = {'staticPlot': False,'displayModeBar': False}
+
 # Apply search filters and return filtered dataset
 def search_dataframe(df: pd.DataFrame, label='Поиск') -> pd.DataFrame:
-
     df = df.copy()
-
     user_text_input = st.text_input(label, placeholder='Введите текст', help='Укажите текст, который могут содержать интересующие Вас проекты')
-
     if user_text_input:
         _user_text_input = "".join([char for char in user_text_input if char.isalnum()])
         mask = df.apply(lambda x: x.astype(str).str.contains(_user_text_input, na=False, flags=re.IGNORECASE))
         df = df.loc[mask.any(axis=1)]
-
-    return df
-
-# Apply filters and return filtered dataset
-def filter_dataframe(df: pd.DataFrame, cols_to_ignore: list) -> pd.DataFrame:
-
-    df = df.copy()
-
-    # Try to convert datetimes into a standard format (datetime, no timezone)
-    for col in df.columns:
-        if is_object_dtype(df[col]):
-            try:
-                df[col] = pd.to_datetime(df[col])
-            except Exception:
-                pass
-
-        if is_datetime64_any_dtype(df[col]):
-            df[col] = df[col].dt.tz_localize(None)
-    
-    modification_container = st.container()
-
-    with modification_container:
-        cols = [col for col in df.columns if col not in cols_to_ignore]
-        to_filter_columns = st.multiselect("Параметры фильтрации", cols)
-        for column in to_filter_columns:
-            left, right = st.columns((1, 20))
-            left.write("└")
-            if 'Менеджер' in df[column].name or 'Курирующий' in df[column].name:
-                options = pd.Series([x for _list in df[column][df[column].notna()] for x in _list]).unique()
-                user_cat_input = right.multiselect(
-                    f"{column}",
-                    options,
-                )
-                if user_cat_input:
-                    df = df[df[column].astype(str).str.contains('|'.join(user_cat_input))]
-            elif is_numeric_dtype(df[column]):
-                _min = float(df[column].min())
-                _max = float(df[column].max())
-                step = 1.00
-                user_num_input = right.slider(
-                    f" {column}",
-                    min_value=_min,
-                    max_value=_max,
-                    value=(_min, _max),
-                    step=step,
-                )
-                df = df[df[column].between(*user_num_input)]
-            elif is_datetime64_any_dtype(df[column]):
-                user_date_input = right.date_input(
-                    f" {column}",
-                    value=(
-                        df[column].min(),
-                        df[column].max(),
-                    ),
-                )
-                if len(user_date_input) == 2:
-                    user_date_input = tuple(map(pd.to_datetime, user_date_input))
-                    start_date, end_date = user_date_input
-                    df = df.loc[df[column].between(start_date, end_date)]
-            elif (is_categorical_dtype(df[column]) or df[column].nunique() < 10 or df[column].map(len).max() < 255) and ('Название проекта' not in df[column].name):
-                options = df[column].unique()
-                user_cat_input = right.multiselect(
-                    f"{column}",
-                    options,
-                )
-                if user_cat_input:
-                    _cat_input = user_cat_input
-                    df = df[df[column].isin(_cat_input)]
-            else:
-                user_text_input = right.text_input(
-                    f"{column}",
-                )
-                if user_text_input:
-                    df = df[df[column].astype(str).str.contains(user_text_input, na=False, flags=re.IGNORECASE)]
-
-    # Try to convert datetimes into displayable date formats
-    for col in df.columns:
-        if is_datetime64_any_dtype(df[col]):
-            df[col] = df[col].dt.strftime('%d-%m-%Y')
-
     return df
 
 # Apply filters and return company name
 def company_selection(df: pd.DataFrame):
     df = df[['ID компании', 'Название компании', 'Тип компании', 'Отрасль']].copy()
+    df.sort_values(by='ID компании', inplace=True)
     df.insert(0, 'Составной ключ', df['ID компании'].astype('str') + ' - ' + df['Название компании'])
     company = False
-
     modification_container = st.container()
     with modification_container:
-        left, right = st.columns(2)
-        # Filters for household name selection input
-        ## df.columns[1:] so that the company name is not used (its the first col)
-        for idx, column in enumerate(df.columns[3:]):
-            options = df[column].unique()
-            ### preselection tweak to preserve selected filter values in case related filters get adjusted
-            cached_value_key = column+'-input'
-            preselection = []
-            if cached_value_key in session:
-                for i in session[cached_value_key]:
-                    try:
-                        if i in options:
-                            preselection.append(i)
-                    except:
-                        pass
-            ### display every other input field on the right column, all the rest - on the left column
-            col = left if idx % 2 == 0 else right
-            user_cat_input = col.multiselect(
-                f"{column}",
-                options,
-                default=preselection,
-                key=cached_value_key,
-            )
-            if user_cat_input:
-                df = df[df[column].isin(user_cat_input)]
         options = np.insert(df['Составной ключ'].unique(), 0, 'Не выбрано', axis=0)
-
         # Household name selection
-        ## preselection tweak once again to preserve selected company in case related filters get adjusted
         preselection = 0
         if 'company_selectbox' in session:
             try:
                 preselection = int(np.where(options == session['company_selectbox'])[0][0])
             except:
                 pass
-
         user_cat_input = st.selectbox(
             "Название компании",
             options,
@@ -170,7 +56,6 @@ def company_selection(df: pd.DataFrame):
         )
         if user_cat_input and user_cat_input != 'Не выбрано':
             company = user_cat_input
-
     return company
 
 # App launch
@@ -207,7 +92,7 @@ def run():
         # О компании
         with tab1:
             #INFO
-            col1, col2 = st.columns(2)
+            col1, col2 = st.columns([4,1])
             for key, value in company_data_df.items():
                 key = key.casefold()
                 value = list(value.values())[0]
@@ -215,7 +100,7 @@ def run():
                     col1.markdown(f'[{value}]({value})')
                 elif 'логотип' in key:
                     try:
-                        col2.image(value, width=300)
+                        col2.image(value, use_column_width='auto')
                     except:
                         col2.caption('Логотип уехал в отпуск')
                 elif 'название компании' in key:
